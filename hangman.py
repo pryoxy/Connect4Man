8c5f63fc6b948c1efcfae2c58ea15e798165006e
Tharanie <tharanie3@yahoo.com>
2023-04-14 20:51:00 -0400

    class makin part 1

--- hangman.py
+++ hangman.py
@@ -1,6 +1,12 @@
 print('hello')
 
-#prompting the person to enter in 5 words
-#prompts the other person to enter in 5 words
+#prompting the person to enter in 5 words 
 #sets of each word is stored as a string. Each set of strings is stored in its own array
 # while loop that continuously prompts for user input for guessing words
+
+class Hangman:
+    
+    def __init__(self,words):  #words is an array of the phrases that the user enter
+        self.wordlist = words
+        
+
