1aa1d0b5f4f8918a68ac2dee32e500fd08f4bea9
Tharanie <tharanie3@yahoo.com>
2023-04-14 20:20:09 -0400

    Project Start

---
+++ hangman.py
@@ -0,0 +1,6 @@
+print('hello')
+
+#prompting the person to enter in 5 words
+#prompts the other person to enter in 5 words
+#sets of each word is stored as a string. Each set of strings is stored in its own array
+# while loop that continuously prompts for user input for guessing words
