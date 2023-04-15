e137c6bb8df473db635e221439a262b5a34db38b
Daniel AlvSeg <daniel.alvaradosegura@gmail.com>
2023-04-14 21:43:38 -0400

    class makin part 2

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+import time
+
 print('hello')
 
 #prompting the person to enter in 5 words 
@@ -6,7 +8,32 @@ print('hello')
 
 class Hangman:
     
-    def __init__(self,words):  #words is an array of the phrases that the user enter
-        self.wordlist = words
+    def __init__(self,word):  #word is the word the user inputs
+        self.word = word
+
+    def play_game(self):    #starts playing game
+        guesses = ''    #guesses of player
         
+        print('Start guessing') 
+
+        while 1:
+            for char in self.word:  #checks guesses and compares to word
+                if char in guesses:
+                    print(char,end=''), #print guessed characters
+                else:
+                    print('_',end=''),  #print dash for unknown
+                    failed += 1
+            
+            if failed == 0:   #player won 
+                print('won')
+                break
+
+            guess = input('guess a letter')
+            guesses += guess
+
+            if guess not in self.word:
+                print('wrong')
+                
+
+
 
